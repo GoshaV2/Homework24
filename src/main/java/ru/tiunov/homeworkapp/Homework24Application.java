b8a9c42ee0509582f63a37fcebb02cd675459b45
Тиунов Георгий <34783tgs@mail.ru>
2022-12-07 17:00:57 +0300

    add new application

---
+++ src/main/java/ru/tiunov/homeworkapp/Homework24Application.java
@@ -0,0 +1,13 @@
+package ru.tiunov.homeworkapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework24Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework24Application.class, args);
+    }
+
+}
